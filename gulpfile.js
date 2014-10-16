f1ac1d818a026635f39de1065cb12f83e2dd21b2
Luciano Simo <lucianosimo87@gmail.com>
2014-10-16 17:29:26 +0200

    Test commit for PR

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint');
 
-gulp.task('js', function() {
-	return gulp.src('comics/*.js')
+gulp.task('comics-js', function() {
+	return gulp.src('comics/**/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		.pipe(gulp.dest('build'));
